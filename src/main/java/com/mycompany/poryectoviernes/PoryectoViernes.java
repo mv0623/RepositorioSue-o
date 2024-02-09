8f2b8a196ed7a77ebb923095fcd15033648e400b
mv0623 <156902469+mv0623@users.noreply.github.com>
2024-02-09 09:14:49 +0100

    Update PoryectoViernes.java

--- src/main/java/com/mycompany/poryectoviernes/PoryectoViernes.java
+++ src/main/java/com/mycompany/poryectoviernes/PoryectoViernes.java
@@ -11,6 +11,6 @@ package com.mycompany.poryectoviernes;
 public class PoryectoViernes {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! Tengo sueño");
     }
 }
